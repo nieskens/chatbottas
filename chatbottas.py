da81634f14fecb00b6e250dc380ae0cde57005ca
Berry Nieskens <berry@nieskens.com>
2019-05-25 21:33:39 +0200

    Added: log all bot responses to list for future reference

--- chatbottas.py
+++ chatbottas.py
@@ -7,21 +7,28 @@ def stripPunctuation(message):
     else:
         return message
 
+conv = conversation.Conversation()
+
 def dialogue():
+    global conv
     # take user's message from input, make lowercase and strip punctuation
     userMessage = input()
     userMessage = userMessage.lower()
     userMessage = stripPunctuation(userMessage)
     
+    # create a new conversation unless the message is "start over"
     # create objects to check for standard messages and patterns
-    conv = conversation.Conversation(userMessage)
-    botResponse = conv.checkMessage()
+    if userMessage == "start over":
+        conv = conv.startOver()
+    botResponse = conv.checkMessage(userMessage)
 
     # respond according to results
     if botResponse:
         print("BOT: {}".format(botResponse))
     else:
         print("BOT: {}".format(random.choice(["Ok","Hmmm","That's interesting"]))) 
+        print("***")
+        print("Answers given so far: ", conv.responsesGiven)
 
 while True:
     dialogue()
--- conversation.py
+++ conversation.py
@@ -1,11 +1,10 @@
 import random, re
 
 class Conversation(object):
-    def __init__(self,message):
-        self.userMessage = message
+    def __init__(self):
         # greetings to be recognized
         self.greetings = ["hi","hello","goodday","what's up","sup","yo","hi there","hey there","hey","greetings earthling","greetings"]
-        self.goodbye = ["goodbye","bye","bye bye","so long","take care","salut","see ya","see you","see ya later","see you later","ttyl","talk to you later","we'll talk later","i gotta go","i have to go","have to go now"]
+        self.goodbye = ["goodbye","bye","bye bye","so long","take care","salut","see ya","see you","see ya later","see you later","ttyl","talk to you later","we'll talk later","i gotta go","i gotta go now","i have to go","have to go now"]
 
         # a collection of responses to a recognized greeting
         self.responses = {
@@ -16,34 +15,49 @@ class Conversation(object):
         self.messages = {
             "how are you":["I'm good, thanks for asking!", "I'm fine, thanks!"]
         }
-        # the responses that the chatbot has given (to prevent repetition of responses)
-        self.responsesGiven = []
-
+        
         # patterns to recognize in a user's statement and their responses
         self.patterns = {
             "I feel (.*)":["Why do you feel {}?", "You feel {}, why?","How long have you been feeling {}?"],
             "Do you remember (.*)":["Of course I remember {}","Yes, I do remember {}","Now that you mention it, I do remember {}"]
         }
 
-    def checkPattern(self):
+        # the responses that the chatbot has given (to prevent repetition of responses)
+        self.responsesGiven = []
+
+    def startOver():
+        self.responsesGiven = []
+
+    def addResponse(self,response):
+        self.responsesGiven.append(response)
+
+    def checkPattern(self,message):
         for pattern in self.patterns:
-            match = re.search(pattern.lower(), self.userMessage)
+            match = re.search(pattern.lower(), message)
             if match:
-                answer = random.choice(self.patterns[pattern])
-                return answer.format(match.group(1))
+                response = random.choice(self.patterns[pattern])
+                answer = response.format(match.group(1))
+                self.addResponse(answer)
+                return answer
             else:
                 return False
 
-    def checkMessage(self):
+    def checkMessage(self,message):
         # If user says hi, return with a greeting from the responses dictionary
-        if self.userMessage in self.greetings:
-            return random.choice(self.responses["greetings"])
-        # If user says hi, return with a greeting from the responses dictionary
-        elif self.userMessage in self.goodbye:
-            return random.choice(self.responses["goodbye"])
+        if message in self.greetings:
+            answer = random.choice(self.responses["greetings"])
+            self.addResponse(answer)
+            return answer
+        # If user says bye, return with a goodbye from the responses dictionary
+        elif message in self.goodbye:
+            answer = random.choice(self.responses["goodbye"])
+            self.addResponse(answer)
+            return answer
         # Else, check for standard messages
-        elif self.userMessage in self.messages:
-            return random.choice(self.messages[self.userMessage])
+        elif message in self.messages:
+            answer = random.choice(self.messages[message])
+            self.addResponse(answer)
+            return answer
         # Else, check for a pattern
         else:
-            return self.checkPattern()
+            return self.checkPattern(message)
