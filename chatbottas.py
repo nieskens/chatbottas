d1e761cd92be89b9f9600a3b8166e0cc0fb01fef
Berry Nieskens <berry@nieskens.com>
2019-05-25 20:35:40 +0200

    split greetings and responses

--- chatbottas.py
+++ chatbottas.py
@@ -1,13 +1,25 @@
 import checkmessage, checkpattern, random, re
 
+def stripPunctuation(message):
+    if message.endswith(('!','?')):
+        message = message[0:-1]
+        return message
+    else:
+        return message
 
 def dialogue():
+    # take user's message from input, make lowercase and strip punctuation
     userMessage = input()
     userMessage = userMessage.lower()
+    userMessage = stripPunctuation(userMessage)
+    
+    # create objects to check for standard messages and patterns
     messageObj = checkmessage.CheckMessage(userMessage)
     messageResult = messageObj.checkMessage()
     patternObj = checkpattern.CheckPattern(userMessage)
     patternResult = patternObj.checkPattern()
+
+    # respond according to results
     if messageResult:
         print("BOT: {}".format(messageResult))
     elif patternResult:
--- checkmessage.py
+++ checkmessage.py
@@ -3,13 +3,25 @@ import random
 class CheckMessage(object):
     def __init__(self,message):
         self.user_message = message
-        self.messages = {
-            "hi":["Hello, how are you?","Hey,you again!", "How have you been?","Goodday, what's up?"],
-            "how are you?":["I'm good, thanks for asking!", "I'm fine, thanks!"]
+        # greetings to be recognized
+        self.greetings = ["hi","hello","goodday","what's up","sup","yo","hi there","hey there","hey","greetings earthling","greetings"]
+        # a collection of responses to a recognized greeting
+        self.responses = {
+            "greetings":["Hello, how are you?","Hey,you again!", "Hi, how have you been?","Goodday, what's up?"]
         }
+        # a collection of standard sentences and a number of responses to choose from
+        self.messages = {
+            "how are you":["I'm good, thanks for asking!", "I'm fine, thanks!"]
+        }
+        # the responses that the chatbot has given (to prevent repetition of responses)
+        self.responsesGiven = []
 
     def checkMessage(self):
-        if self.user_message in self.messages:
+        # If user says hi, return with a greeting from the responses dictionary
+        if self.user_message in self.greetings:
+            return random.choice(self.responses["greetings"])
+        # Else, check for 
+        elif self.user_message in self.messages:
             return random.choice(self.messages[self.user_message])
         else:
             return False
